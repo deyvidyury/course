ca06f48640c75df80acce14ff193682bbba04976
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-06-10 00:02:10 -0300

    Service layer, component registration

---
+++ src/main/java/com/yury/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.yury.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yury.course.entities.User;
+import com.yury.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
